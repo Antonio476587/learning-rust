64d6b044161c099e30abdb921c3e2fc7db56190f
fantonix <fantonix.web@gmail.com>
2022-12-12 16:36:56 -0400

    Learning about the Rust enumerations (enum) and the Option enum loaded in the prelude with the types None and Some.

---
+++ chapter_6/enumerations/src/main.rs
@@ -0,0 +1,68 @@
+fn main() {
+    enum IpAddressKind {
+        V4,
+        V6,
+    }
+
+    struct IpAddress {
+        kind: IpAddressKind,
+        address: String,
+    }
+
+    let home = IpAddress {
+        kind: IpAddressKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddress {
+        kind: IpAddressKind::V6,
+        address: String::from("::1"),
+    };
+
+    // Adding types to the Enum, it could be any type
+
+    enum IpAddr {
+        V4(u8, u8, u8, u8),
+        V6(String),
+    }
+
+    let home = IpAddr::V4(127, 0, 0, 1);
+
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    // Differences and similarities between enum and structs
+
+    // Enum message types
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+
+    // Struct message types
+    struct QuitMessage; // unit struct
+    struct MoveMessage {
+        x: i32,
+        y: i32,
+    }
+    struct WriteMessage(String); // tuple struct
+    struct ChangeColorMessage(i32, i32, i32); // tuple struct
+
+    // The Enums can have implementations too
+    impl Message {
+        fn call(&self) {
+            // method body would be defined here
+        }
+    }
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+
+    // Using the important Options enum
+
+    let some_number = Some(5);
+    let some_char = Some('e');
+
+    let absent_number: Option<i32> = None;
+}
