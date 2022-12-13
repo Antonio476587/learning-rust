f3c6ed2fa262ab16f435772effcf5dcd803cbaef
fantonix <fantonix.web@gmail.com>
2022-12-13 18:04:56 -0400

    Learning about Rust's Vectors.

---
+++ chapter_8/vectors/src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    let mut v: Vec<i32> = Vec::new();
+
+    v.push(4);
+    // Creating a vector with the macro
+    let v = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v[2];
+    println!("The third element is {}", third);
+
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(third) => println!("The third element is {}", third),
+        None => println!("There is no third element."),
+    }
+
+    // Because vectors only can have one type, enums can 'hack' that behavior
+
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+}
