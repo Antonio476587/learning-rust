09e96ee2f1b4fed33e901a50fbbdf182214dc761
fantonix <fantonix.web@gmail.com>
2022-12-20 15:49:17 -0400

    New traits, traits bounds, blanket implementation and jesus

---
+++ chapter_10/traits/src/main.rs
@@ -0,0 +1,67 @@
+use std::fmt::{Display, Debug};
+
+use traits::aggregator::{Summary, Tweet, NewsArticle, notify};
+
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+
+    //  Using default Summary's implementation
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best \
+             hockey team in the NHL.",
+        ),
+    };
+
+    println!("New article available! {}", article.summarize());
+    notify(&article);
+}
+
+// where clause
+fn some_function<T, U>(t: &T, u: &U) -> i32
+where
+    T: Display + Clone,
+    U: Clone + Debug,
+{
+    0
+}
+
+// Implementation in generics that implement the Display and PartialOrd traits
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
+
+// blanket implementations
+impl<B: Display> ToString for B {
+    // --snip--
+}
